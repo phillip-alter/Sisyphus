1e7f1f3e3903d252d2f5aa98ba932d82af7279ab
Phillip Alter <phillip.alter@outlook.com>
2025-09-08 20:12:11 -0700

    feat: implement initial ToDoEr addon structure with frame and minimap button

---
+++ ToDoEr.lua
@@ -0,0 +1,80 @@
+---
+--- DB Creation
+---
+
+if not ToDoErDB then
+    ToDoErDB = {}
+end
+
+ToDoEr = ToDoEr or {}
+
+---
+--- Frame Creation
+--- 
+
+local listFrame = CreateFrame("Frame","ToDoErFrame",UIParent,"BasicFrameTemplateWithInset")
+listFrame:SetSize(350,700)
+listFrame:SetPoint("CENTER",UIParent,"CENTER",0,0)
+listFrame.TitleBg:SetHeight(30)
+listFrame.title = listFrame:CreateFontString(nil,"OVERLAY","GameFontHighlight")
+listFrame.title:SetPoint("TOPLEFT",listFrame.TitleBg,"TOPLEFT",5,-3)
+listFrame.title:SetText("ToDo 'Er")
+listFrame:Hide()
+listFrame:EnableMouse(true)
+listFrame:SetMovable(true)
+listFrame:RegisterForDrag("LeftButton")
+listFrame:SetScript("OnDragStrat",function(self)
+    self:StartMoving()
+end)
+listFrame:SetScript("OnDragStop",function(self)
+    self:StopMovingOrSizing()
+end)
+
+SLASH_TODOER1 = "/todoer"
+SLASH_TODOER2 = "/tde"
+SlashCmdList["TODOER"] = function()
+    if listFrame:IsShown() then
+        listFrame:Hide()
+    else
+        listFrame:Show()
+    end
+end
+
+table.insert(UISpecialFrames, "MyAddonMainFrame")
+
+---
+--- Minimap Button
+---
+
+local addon = LibStub("AceAddon-3.0"):NewAddon("ToDoEr")
+ToDoErMinimapButton = LibStub("LibDBIcon-1.0",true)
+
+local minibutton = LibStub("LibDataBroker-1.1"):NewDataObject("ToDoEr",{
+    type = "data source",
+    text = "ToDoEr",
+    icon = "Interface\\Addons\\ToDoEr\\minimap.tga",
+    OnClick = function(self,btn)
+        if btn == "LeftButton" then
+            ToDoEr:ToggleMainFrame()
+        end
+    end,
+    OnTooltipShow = function(tooltip)
+        if not tooltip or not tooltip.AddLine then
+            return
+        end
+        tooltip:AddLine("ToDoEr\n\nLeft-Click: Open ToDoEr",nil,nil,nil,nil)
+    end,
+})
+
+function addon:OnInitialize()
+    self.db = LibStub("AceDB-3.0"):New("ToDoErMinimapPOS",{
+        profile = {
+            minimap = {
+                hide = false,
+            },
+        },
+    })
+    ToDoErMinimapButton:Register("ToDoEr",minibutton,self.db.profile.minimap)
+end
+
+--ToDoErMinimapButton:Show("ToDoEr")
