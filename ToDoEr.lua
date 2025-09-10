519150d1fbec2d492f0381d30a917114141dd99c
Phillip Alter <phillip.alter@outlook.com>
2025-09-09 18:55:37 -0700

    feat: implement task display frame and update AddItem function for task management

--- ToDoEr.lua
+++ ToDoEr.lua
@@ -16,18 +16,6 @@ if not ToDoErGlobalDB then
 end
 
 ---
---- init DB counters
----
-
-if not ToDoErDB.Count then
-    ToDoErDB.Count = 0
-end
-
-if not ToDoErGlobalDB.Count then
-    ToDoErGlobalDB.Count = 0
-end
-
----
 --- Init Text 
 ---
 
@@ -60,7 +48,7 @@ listFrame.player:SetPoint("CENTER",listFrame,"TOP",0,-50)
 listFrame.player:SetText("Character: " .. UnitName("player") .. " (Level " .. UnitLevel("player") .. ")")
 
 ---
---- Button to Add Tasks
+--- Add Tasks
 ---
 
 local textBox = CreateFrame("EditBox","TextAdd",listFrame,"InputBoxTemplate")
@@ -77,7 +65,12 @@ addButton:SetPoint("CENTER",textBox,0,-40)
 addButton:SetText("Add Task")
 addButton:SetScript("OnEnter", function(self)
     GameTooltip:SetOwner(self,"ANCHOR_RIGHT")
-    GameTooltip:SetText("Add a task to this character.",nil,nil,nil,nil,true)
+    GameTooltip:SetText("Add a task to this character only.",nil,nil,nil,nil,true)
+end)
+addButton:SetScript("OnClick",function()
+    local text = textBox:GetText()
+    AddItem(text)
+    textBox:SetText("")
 end)
 
 
@@ -90,50 +83,92 @@ end)
 --- up arrow to move up on list
 --- down arrow to move down on list 
 
-local function AddItem(text,isGlobal)
-    -- add checkbox 
-    -- add text box
-    -- determine whether account-wide or local
-    -- add task id and text to correct db
-    -- add delete button
-    -- add up arrow
-    -- add down arrow
+local displayFrame = CreateFrame("Frame","ListDisplayFrame",UIParent,"BasicFrameTemplateWithInset")
+displayFrame:SetSize(200,250)
+displayFrame:SetPoint("TOPLEFT",UIParent,"TOPLEFT",0,-150)
+displayFrame.TitleBg:SetHeight(30)
+displayFrame.title = displayFrame:CreateFontString(nil,"OVERLAY","GameFontHighlightSmall")
+displayFrame.title:SetPoint("TOPLEFT",displayFrame.TitleBg,"TOPLEFT",5,-3)
+displayFrame.title:SetText("Tasks")
+displayFrame:EnableMouse(true)
+displayFrame:SetMovable(true)
+displayFrame:SetResizable(true)
+displayFrame:SetAlpha(0.33)
+displayFrame:RegisterForDrag("LeftButton")
+displayFrame:SetScript("OnDragStart",function(self)
+    self:StartMoving()
+end)
+displayFrame:SetScript("OnDragStop",function(self)
+    self:StopMovingOrSizing()
+end)
+
+
+local showButton = CreateFrame("Button","ToDoErShowButton",listFrame, "UIPanelButtonTemplate")
+showButton:SetSize(110,30)
+showButton:SetPoint("BOTTOMRIGHT",listFrame,-10,10)
+showButton:SetText("Show/Hide List")
+showButton:SetScript("OnClick", function()
+    if not displayFrame:IsShown() then
+        displayFrame:Show()
+    else 
+        displayFrame:Hide()
+    end
+end)
+
+
+function AddItem(text)
+    if text and text ~= "" then
+        local taskData = {
+            text = text,
+            checked = false,
+            lastChecked = 0
+        }
+        table.insert(ToDoErDB,taskData)
+        --UpdateList()
+        -- not yet implemented
+    end
 end
 
-local function RemoveItem(id)
+function RemoveItem(id)
     -- remove item from pinned list
     -- remove item from db
     -- update frame
 end
 
-local function MoveUp()
+function MoveUp()
     -- swap this item and previous item, ie swap [1] for [0]
     -- update frame
 end
 
-local function MoveDown()
+function MoveDown()
     -- swap this item and next item, ie swap [0] for [1]
     -- update frame
 end
 
-local function UnCheckAll()
+function UnCheckAll()
     -- set all items to unchecked
+end
+
+
+function UpdateList()
+    --todo 
 end
 
 ---
 --- QOL Features
 ---
 
+--Slash commands for adding/deleting/clearing task list
 
-local function NewDailyReset()
-    -- compare dates of checks to set time (ie next day at 10 AM PT)
-    -- uncheck daily checkboxes
-end
+-- local function NewDailyReset()
+--     -- compare dates of checks to set time (ie next day at 10 AM PT)
+--     -- uncheck daily checkboxes
+-- end
 
-local function NewWeeklyReset()
-    -- compare dates of checks to set time (Tuesday at 9 AM PT)
-    -- uncheck weekly checkboxes
-end
+-- local function NewWeeklyReset()
+--     -- compare dates of checks to set time (Tuesday at 9 AM PT)
+--     -- uncheck weekly checkboxes
+-- end
 
 ---
 --- Slash Commands
@@ -147,6 +182,11 @@ SlashCmdList["TODOER"] = function()
     else
         listFrame:Show()
     end
+end
+
+SLASH_TODOERRESET1 = "/tdereset"
+SlashCmdList["TODOERRESET"] = function()
+    ToDoErDB = {}
 end
 
 table.insert(UISpecialFrames, "ToDoErFrame")
